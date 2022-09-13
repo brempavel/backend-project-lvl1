081cd61b5abf06661e6d3cad44abc9f5f62b2f09
Pavlo Brem <brempavel@gmail.com>
2022-09-13 17:05:04 +0300

    removed console statements

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const greet = () => {
+export default () => {
+  console.log('Welcome to Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
-
-export default greet;
