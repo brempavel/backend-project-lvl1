eabaadad7123c80a353371642d4d720a362afd8e
Pavlo Brem <brempavel@gmail.com>
2022-09-14 13:19:19 +0300

    added new game

---
+++ games/prime.js
@@ -0,0 +1,20 @@
+import { getRandomInt, gameEngine } from '../src/index.js';
+
+export default () => {
+  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const getQuestion = () => {
+    const num = getRandomInt(2, 100);
+    console.log(`Question: ${num}`);
+    return num;
+  };
+  const getRightAnswer = (num) => {
+    const s = Math.floor(Math.sqrt(num)) + 1;
+    for (let i = 2; i <= s; i += 1) {
+      if (num % i === 0) {
+        return 'no';
+      }
+    }
+    return 'yes';
+  };
+  gameEngine(rules, getQuestion, getRightAnswer);
+};
