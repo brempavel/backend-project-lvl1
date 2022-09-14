c7174b57c8be9beabe9066e936d5616644a22918
Pavlo Brem <brempavel@gmail.com>
2022-09-14 14:33:16 +0300

    fixed brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 import { greet } from '../src/index.js';
 
+console.log('Welcome to Brain Games!');
+const greet = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
 greet();
